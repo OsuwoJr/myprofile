95659a1295b2330bbc78981de906d3ab8ec0013d
OsuwoJr <johnfredosuwo@gmail.com>
2024-02-27 00:33:49 +0300

    netlify link changed

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -20,7 +20,7 @@
 {/each}
     </div>
     <a 
-    href="https://osuwo-jr.netlify.app/" target="_blank"
+    href="https://osuwojrr.netlify.app/" target="_blank"
     class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
        <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200">
             <h4 class="relative z-10 ">Get In Touch</h4>
