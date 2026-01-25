cb8bc2c4f51d577139059254d50c0418acedc49f
OsuwoJr <osuwoodongo@gmail.com>
2026-01-25 21:48:59 +0300

    chore: stop committing vercel build output

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.ClAvzu4g.js","_app/immutable/chunks/C-tmgscj.js","_app/immutable/chunks/CWIBN2Iv.js","_app/immutable/chunks/Dj-PIELV.js","_app/immutable/chunks/CW3eGBZa.js"];
-export const stylesheets = ["_app/immutable/assets/0.9T9V4Ubc.css"];
-export const fonts = [];
